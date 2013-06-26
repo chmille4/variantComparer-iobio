14ab8754a5a5f30764039e0bcd14564e8849fff3
Chase Miller <chmille4@gmail.com>
2013-06-26 16:33:21 -0400

    reorganize to remove public folder

--- pageServer.js
+++ pageServer.js
@@ -13,7 +13,7 @@ var io = require('socket.io').listen(server);
 server.listen(8070);
 
 // setup server
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname));
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
